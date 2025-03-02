b3b54f2b28b3fb2b0adc913f374b93dd73d98c2f
Neeraj9080 <neerajpatel02468@gmail.com>
2025-03-02 22:00:51 +0530

    [Neeraj] Add. Create Rest Controller for various HTTP methods and test REST calls using CURL

---
+++ AddressBook/src/main/java/controller/AddressBookController.java
@@ -0,0 +1,45 @@
+package controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/addressbook")
+public class AddressBookController {
+
+    private final AddressBookService addressBookService;
+
+    @Autowired
+    public AddressBookController(AddressBookService addressBookService) {
+        this.addressBookService = addressBookService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AddressBookDTO>> getAllAddressBook() {
+        return ResponseEntity.ok(addressBookService.getAllAddressBook());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AddressBookDTO> getAddressBookById(@PathVariable Long id) {
+        return ResponseEntity.ok(addressBookService.getAddressBookById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<AddressBookDTO> createAddressBook(@RequestBody AddressBookDTO addressBookDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(addressBookService.createAddressBook(addressBookDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AddressBookDTO> updateAddressBook(@PathVariable Long id, @RequestBody AddressBookDTO addressBookDTO) {
+        return ResponseEntity.ok(addressBookService.updateAddressBook(id, addressBookDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteAddressBook(@PathVariable Long id) {
+        addressBookService.deleteAddressBook(id);
+        return ResponseEntity.noContent().build();
+    }
+}
